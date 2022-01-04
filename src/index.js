c329f253b9fdd05ddfe49191dce63474eae832d7
helgafromspace <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>
2022-01-04 16:53:16 +0200

    init: start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
-}
+    if (n > 0){
+        return n.toString().split('').reverse().join('');
+        }
+      else {
+        num = n.toString().split('');
+        num.shift();
+        return num.reverse().join('');
+      }
+    }
